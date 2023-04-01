2c02c90d767d1bf5b12c45732c266cf4de591db5
amirtorkama5204@gmail.com <amirtorkaman5204@gmail.com>
2023-04-01 11:17:26 +0330

    refactor: remove unused callback

--- nested-set.php
+++ nested-set.php
@@ -15,32 +15,3 @@ include "includes/functions.php";
 include "includes/move-terms.php";
 include "includes/class-nested-term-hooks.php";
 include "includes/admin/class-nested-admin.php";
-
-add_action( 'init', 'amir_add_nested_set' );
-
-
-function amir_add_nested_set() {
-	if ( isset( $_GET['move'] ) ) {
-//		nested_move_terms();
-
-//		$install = new Nested_Term_Install();
-//		$install->fix_tree();
-		exit();
-	}
-
-	if ( ! isset( $_GET['nested'] ) ) {
-		return false;
-	}
-
-
-	echo_pre( nested_get_ancestors( 10 ) );
-
-	die();
-}
-
-
-function echo_pre( $value ) {
-	echo "<pre>";
-	print_r( $value );
-	echo "</pre>";
-}
